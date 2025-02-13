efa1d32fdd2b2ddfee9136f2c0e20fb7c585b246
satrya070 <satrya@spotr.ai>
2025-02-13 15:49:39 +0100

    add cmakelists

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+// UdemyOpenGL.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+//#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+//#include <glm/glm.hpp>
+
+void framebuffer_resize_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 600;
+
+int main()
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "glTF renderer", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+
+    // initialize glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "failed to init GLAD" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        //processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        // clear all
+        glfwTerminate();
+        return 0;
+    }
+
+    std::cout << "Hello World!\n";
+}
+
+void framebuffer_resize_callback(GLFWwindow* window, int widht, int height) {
+}
