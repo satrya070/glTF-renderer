27b7d616ccf7368de9215be5c71ffeb2628919fa
satrya070 <satrya@spotr.ai>
2025-02-16 19:53:10 +0100

    use load

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,28 @@ void processInput(GLFWwindow* window);
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
 
+bool loadModel(tinygltf::Model& model, const char* filename) {
+    tinygltf::TinyGLTF loader;
+    std::string err;
+    std::string warn;
+
+    bool res = loader.LoadASCIIFromFile(&model, &err, &warn, filename);
+    if (!warn.empty()) {
+        std::cout << "WARNING: " << warn << std::endl;
+    }
+
+    if (!err.empty()) {
+        std::cout << "ERR: " << warn << std::endl;
+    }
+
+    if (!res)
+        std::cout << "Failed to load glTF: " << filename << std::endl;
+    else
+        std::cout << "Loaded glTF: " << filename << std::endl;
+
+    return res;
+}
+
 int main()
 {
     GLFWwindow* window;
@@ -42,6 +64,17 @@ int main()
         std::cout << "failed to init GLAD" << std::endl;
         return -1;
     }
+
+    std::string filename = "../../../models/lowpoly_tree/result.gltf";
+
+    // ----- gltf model
+    tinygltf::Model model;
+    loadModel(model, filename.c_str());
+    // model info
+    std::cout << "n meshes: " << model.meshes.size() << std::endl;
+    std::cout << "n nodes: " << model.nodes.size() << std::endl;
+    std::cout << "number materials: " << model.materials.size();
+    
 
     while (!glfwWindowShouldClose(window))
     {
